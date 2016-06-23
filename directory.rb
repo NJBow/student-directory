0dd0b34283f6bf61e5b847b972af220ffd42dd41
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-23 08:37:25 +0000

    Overall and notes

--- directory.rb
+++ directory.rb
@@ -11,3 +11,10 @@ puts "Freddy Kroger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+print "Overall, we have "
+print 11
+puts " great students"
+
+#puts has return at the end
+#print does not have return
+#() are not necessary when quoting an argument
