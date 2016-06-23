8b3a147e319a28d1dac715f3d4ebddc0a82d46d9
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-23 09:17:13 +0000

    String interpolation of student_count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villains Academy"
 puts "---------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,9 +13,7 @@ puts "Freddy Kroger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
 
 #puts has return at the end
 #print does not have return
