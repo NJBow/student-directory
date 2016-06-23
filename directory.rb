ce1bc65f94a9f2c590fa47e729ba4fc37541a234
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-23 09:42:32 +0000

    Iteration and Arrays

--- directory.rb
+++ directory.rb
@@ -1,19 +1,17 @@
-student_count = 11
+students_array = ["Dr. Hannibal Lecter","Darth Vader",
+"Nurse Ratched","Michael Corleone",
+"Alex Delarge","The Wicked Witch of the West",
+"Terminator","Freddy Kroger",
+"The Joker","Joffrey Baratheon",
+"Norman Bates"
+    ]
 
 puts "The students of Villains Academy"
 puts "---------------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kroger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Overall, we have #{student_count} great students"
+students_array.each do |student|
+    puts student
+end
+puts "Overall, we have #{students_array.count} great students"
 
 #puts has return at the end
 #print does not have return
